4b5348c5c766e717f7547f87396b9d22507590ac
Felicia <felicia.d878@gmail.com>
2026-08-19 22:28:42 +0800

    Update greeting and exit

---
+++ src/main/java/Koko.java
@@ -0,0 +1,12 @@
+public class Koko {
+    public static void main(String[] args) {
+        String banner = " _  __     _          \n"
+                + "| |/ /___ | | _____   \n"
+                + "| ' // _ \\| |/ / _ \\  \n"
+                + "| . \\ (_) |   < (_) | \n"
+                + "|_|\\_\\___/|_|\\_\\___/  \n";
+        System.out.println(banner);
+        System.out.println("hello! i am koko. how can i help you?\n" +
+                "bye, hope to see you again soon!");
+    }
+}
